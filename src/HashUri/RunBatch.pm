4e483a1f2afbc93517cb70d3713122392e9f13ff
Tobias Kuhn <kuhntobias@gmail.com>
2013-11-29 10:14:53 +0100

    Implement RunBatch

---
+++ src/HashUri/RunBatch.pm
@@ -0,0 +1,42 @@
+package HashUri::RunBatch;
+
+use strict;
+use warnings;
+use HashUri::CheckFile;
+use HashUri::File::ProcessFile;
+use LWP::Simple;
+use Time::HiRes;
+
+our $VERSION = '0.01';
+
+sub run {
+	my $file_name = shift;
+	$file_name or die "No file name given";
+	open my $file, $file_name or die "Could not open $file_name: $!";
+	while( my $line = <$file>)  {
+		$line =~ s/^\s+|\s+$//g;
+		print "COMMAND: $line\n";
+		my @cmdargs  = split(' ', $line);
+		my $cmd = shift @cmdargs;
+		my $starttime = [Time::HiRes::gettimeofday()];
+		eval {
+			if ($cmd eq "CheckFile") {
+				HashUri::CheckFile::check(@cmdargs);
+			} elsif ($cmd eq "ProcessFile") {
+				HashUri::File::ProcessFile::process(@cmdargs);
+			} else {
+				print "ERROR: Unrecognized command $cmd";
+				exit 1;
+			}
+		} or do {
+			my $m = $@;
+			print "$m";
+		};
+		my $t = Time::HiRes::tv_interval($starttime);
+		print "Time in seconds: $t\n";
+		print "---\n";
+		last if $. == 2;
+	}
+}
+
+1;
